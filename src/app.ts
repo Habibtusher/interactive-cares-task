bd17872fc317e9b7dd28311d6bcbd98033e30a70
Habibtusher <tusherhrt@gmail.com>
2024-04-09 23:38:50 +0600

    main route success message

--- src/app.ts
+++ src/app.ts
@@ -23,15 +23,10 @@ app.use(globalErrorHandler);
 
 //handle not found
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.status(httpStatus.NOT_FOUND).json({
-    success: false,
-    message: 'Not Found',
-    errorMessages: [
-      {
-        path: req.originalUrl,
-        message: 'API Not Found',
-      },
-    ],
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: 'Orders services working',
+
   });
   next();
 });
